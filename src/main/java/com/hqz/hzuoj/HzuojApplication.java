dbc6b950a22156568d83ac132769c7bd4288af1f
Cruiying <2322144259@qq.com>
2020-06-27 13:53:10 +0800

    commit

---
+++ src/main/java/com/hqz/hzuoj/HzuojApplication.java
@@ -0,0 +1,13 @@
+package com.hqz.hzuoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzuojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HzuojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hqz/hzuoj/HzuojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hqz.hzuoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HzuojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
