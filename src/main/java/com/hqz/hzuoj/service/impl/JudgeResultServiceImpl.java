1a98bf66e2b369ffd049d80404258b10befb0001
Cruiying <2322144259@qq.com>
2020-07-13 10:11:54 +0800

    添加测评结果

---
+++ src/main/java/com/hqz/hzuoj/service/impl/JudgeResultServiceImpl.java
@@ -0,0 +1,88 @@
+package com.hqz.hzuoj.service.impl;
+
+import com.hqz.hzuoj.entity.model.JudgeResult;
+import com.hqz.hzuoj.mapper.JudgeResultMapper;
+import com.hqz.hzuoj.service.JudgeResultService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * (JudgeResult)表服务实现类
+ *
+ * @author Cruiying
+ * @since 2020-06-22 21:17:30
+ */
+@Service("judgeResultService")
+public class JudgeResultServiceImpl implements JudgeResultService {
+
+    @Resource
+    private JudgeResultMapper judgeResultMapper;
+
+    /**
+     * 通过ID查询单条数据
+     *
+     * @param judgeResultId 主键
+     * @return 实例对象
+     */
+    @Override
+    public JudgeResult queryById(Integer judgeResultId) {
+        return this.judgeResultMapper.queryById(judgeResultId);
+    }
+
+
+    /**
+     * 新增数据
+     *
+     * @param judgeResult 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public JudgeResult insert(JudgeResult judgeResult) {
+        this.judgeResultMapper.insert(judgeResult);
+        return judgeResult;
+    }
+
+    /**
+     * 修改数据
+     *
+     * @param judgeResult 实例对象
+     * @return 实例对象
+     */
+    @Override
+    public JudgeResult update(JudgeResult judgeResult) {
+        this.judgeResultMapper.update(judgeResult);
+        return this.queryById(judgeResult.getJudgeResultId());
+    }
+
+    /**
+     * 通过主键删除数据
+     *
+     * @param judgeResultId 主键
+     * @return 是否成功
+     */
+    @Override
+    public boolean deleteById(Integer judgeResultId) {
+        return this.judgeResultMapper.deleteById(judgeResultId) > 0;
+    }
+
+    /**
+     * 获取所有测评列表
+     * @return
+     */
+    @Override
+    public List<JudgeResult> findJudgeResults() {
+        return judgeResultMapper.findJudgeResults();
+    }
+
+    /**
+     * 根据测评缩写名称查找测评结果
+     * @param judgeNameAbbr
+     * @return
+     */
+    @Override
+    public JudgeResult findJudgeResultByJudgeNameAbbr(String judgeNameAbbr) {
+        return judgeResultMapper.findJudgeResultByJudgeNameAbbr(judgeNameAbbr);
+    }
+}
